8641aba0cacb276f3dc46722943d8a9934d3f4ea
Arthur Juchereau <PookMook@users.noreply.github.com>
2016-06-21 14:08:44 -0400

    sqlite populate

--- config.ini.php
+++ config.ini.php
@@ -28,14 +28,25 @@ try {
         $file_db->exec("CREATE TABLE IF NOT EXISTS swagornot (id_son INTEGER PRIMARY KEY, owner INTEGER)");
         $file_db->exec("CREATE TABLE IF NOT EXISTS swagornot_picture (id_picture INTEGER PRIMARY KEY,id_son INTEGER, owner INTEGER, picture TEXT)");
         $file_db->exec("CREATE TABLE IF NOT EXISTS swagornot_vote (id_vote INTEGER PRIMARY KEY,id_winner INTEGER,id_loser INTEGER, owner INTEGER, picture TEXT)");
+        $file_db->exec("INSERT INTO user (nom, email) VALUES ('Arthur','arthur@launchleap.com')");
+        $file_db->exec("INSERT INTO user (nom, email) VALUES ('Julien','julien@launchleap.com')");
+        $file_db->exec("INSERT INTO user (nom, email) VALUES ('Thomas','thomas@launchleap.com')");
+        $file_db->exec("INSERT INTO user (nom, email) VALUES ('Charles','charles@launchleap.com')");
   }
 
 //No session initiated
 if(empty($_SESSION['id'])){
-  
-
+    //log in as a random user for test purposes
+    $getUser = $file_db->query("SELECT * FROM user ORDER BY RANDOM() LIMIT 1;");
+    $user = $getUser->fetch(PDO::FETCH_ASSOC);
+    
+    //create new session
+    $newSession = $file_db->prepare("INSERT INTO user_session (id_user) VALUES (:user)");
+    $newSession->bindParam(":user",$user['id_user']);
+    $newSession->execute() or die('Unable to create new session');
+    
+    //populate session var
+    $_SESSION['userID'] = $user['id_user'];
+    $_SESSION['id'] = $file_db->lastInsertId();
 }
-
-
-
 ?>
